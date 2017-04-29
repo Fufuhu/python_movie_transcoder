ddce7f230c52ff532a74b1e1f5b0a5d16fa7bbaf
Fufuhu <e238058r@hotmail.co.jp>
2017-04-29 23:40:44 +0900

    プロジェクト構成を変更

--- modules/setup.py
+++ modules/setup.py
@@ -22,7 +22,7 @@ version = next((line.split('=')[1].strip().replace("'", '')
                                               'python_movie_transformer',
                                               '__init__.py'))
                 if line.startswith('__version__ = ')),
-               '0.0.dev0')
+               '0.0.dev1')
 
 setup(
     name="python_movie_transformer",
