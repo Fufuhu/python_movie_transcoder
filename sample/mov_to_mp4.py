675097275e3f13ea46d8b1eb590df8729052c3cc
Fufuhu <e238058r@hotmail.co.jp>
2017-04-29 20:34:04 +0900

    Deliting sample directory

--- sample/mov_to_mp4.py
+++
@@ -1,7 +0,0 @@
-import ffmpy
-
-ff = ffmpy.FFmpeg(
-    inputs={'/Users/01013548/video/task_property.mov':None},
-    outputs={'output.mp4': None}
-)
-ff.run()
--- sample/resize.py
+++
@@ -1,7 +0,0 @@
-import ffmpy
-
-ff = ffmpy.FFmpeg(
-    inputs={'/Users/01013548/video/task_property.mov':None},
-    outputs={'output.mp4': '-vf scale=320:-1'}
-)
-ff.run()
